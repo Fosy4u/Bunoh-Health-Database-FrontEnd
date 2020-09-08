267159885dd4154a17dd55c9d0b8ad07682f2d3a
Foster Ogwudu <Foster.Ogwudu@yahoo.com>
2020-09-08 03:05:59 +0100

    Updates

---
+++ precache-manifest.d164cf98cf3e72d50fd3e8b3438b9a77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f4a87ec38ba58406ef5dd02ad0d7e4",
+    "url": "/Bunoh-Health-Database-FrontEnd/index.html"
+  },
+  {
+    "revision": "cf960e0b3d100519b197",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/css/2.1638176c.chunk.css"
+  },
+  {
+    "revision": "3b696c684adb08bf854c",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/css/main.e38cd917.chunk.css"
+  },
+  {
+    "revision": "cf960e0b3d100519b197",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/js/2.1a16c029.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/js/2.1a16c029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b696c684adb08bf854c",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/js/main.48a88513.chunk.js"
+  },
+  {
+    "revision": "90b897f037b61642e60c",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/js/runtime-main.02b01444.js"
+  },
+  {
+    "revision": "7599329920bebd2351c7ecba81bb10a4",
+    "url": "/Bunoh-Health-Database-FrontEnd/static/media/logo.75993299.JPG"
+  }
+]);
